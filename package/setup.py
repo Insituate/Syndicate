203482caaad4f6019867012db8960208474634a7
Nishant Sharma <121046929+sevendaystoglory@users.noreply.github.com>
2024-02-09 16:39:58 +0530

    Update setup.py

--- package/setup.py
+++ package/setup.py
@@ -1,1 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="syndicate",
+    version="0.1.0",
+    author="Insituate",
+    author_email="nishant@insituate.ai",
+    description="Empowering enterprises, with the power of open source.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/Insituate/Syndicate",
+    packages=find_packages(),
+    install_requires=[
+        # Add your project's dependencies here
+    ],
+    classifiers=[
+        # Choose your license as you wish
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires='>=3.6',
+)
 
